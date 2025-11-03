09dca15e11bbea749efd2a97226609e6ade7f458
Leighton Grant <oneal.grant@outlook.com>
2025-11-03 16:47:09 +0000

    feat(notes): add individual note viewing functionality

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,5 @@
 const myNotes = document.querySelector('.my-notes')
+const noteView = document.querySelector('.note-view')
 
 const url = 'http://localhost:5000/api/notes'
 const getNotes = async (url) => {
@@ -17,17 +18,45 @@ const renderNotes = (data) => {
 	myNotes.innerHTML = ''
 	let listGroupItems = ''
 	data.forEach((item) => {
-		const { title, noteText } = item
-		return (listGroupItems += `<a href="#" class="list-group-item list-group-item-action rounded-3" aria-current="true">
+		const { title, noteText, id } = item
+		return (listGroupItems += `<div class="list-group-item list-group-item-action rounded-3 note-item" aria-current="true" id="${id}">
 															<div class="d-flex w-100 justify-content-between">
 																<h5 class="mb-1">${title}</h5>
 																<small>3 days ago</small>
 															</div>
-    													<p class="mb-1">${noteText}</p>
+    													<p class="mb-1 text-truncate" style="max-width: 200px;">${noteText}</p>
     													<small>And some small print.</small>
-  												</a>`)
+  												</div>`)
 	})
 	myNotes.innerHTML = listGroupItems
+	const noteItems = document.querySelectorAll('.note-item')
+	noteItems.forEach((item) => {
+		item.addEventListener('click', (e) => {
+			let id = e.target.id
+			if (!id) {
+				id = e.target.parentElement.id || e.target.parentElement.parentElement.id
+			}
+			getNote(id)
+		})
+	})
+}
+
+const getNote = async (id) => {
+	const url = `http://localhost:5000/api/notes/${id}`
+	const response = await fetch(url)
+	if (response.ok) {
+		const note = await response.json()
+		renderNote(note)
+	}
+}
+
+const renderNote = (note) => {
+	const { title, noteText } = note
+	const currentNote = `<h2>${title}</h2>
+											 <p>${noteText}</p>
+											`
+
+	noteView.innerHTML = currentNote
 }
 
 getNotes(url)
--- routes.ts
+++ routes.ts
@@ -2,9 +2,28 @@ import { Router } from 'express'
 import type { Request, Response } from 'express'
 import { readFile } from 'node:fs/promises'
 
+type NotesData = {
+	id: string
+	title: string
+	noteText: string
+	createdAt: string
+	updatedAt: string
+}
+
 export const router = Router()
 
+// Get all notes
 router.get('/notes', async (req: Request, res: Response) => {
 	const notesData = await readFile(new URL('./notes.json', import.meta.url), 'utf-8')
 	res.status(200).json(JSON.parse(notesData))
+})
+
+// Get one note
+router.get('/notes/:id', async (req: Request, res: Response) => {
+	console.log(req.params)
+	const { id } = req.params
+	const notesData = await readFile(new URL('./notes.json', import.meta.url), 'utf-8')
+	const notes = JSON.parse(notesData)
+	const note = notes.find((item: NotesData) => item.id === id)
+	res.status(200).json(note)
 })
