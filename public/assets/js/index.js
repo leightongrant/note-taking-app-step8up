6439fe66585eb32d8b82ffea5518d0e7967444a6
Leighton Grant <oneal.grant@outlook.com>
2025-11-04 11:58:23 +0000

    feat(notes): add note creation functionality with save button

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,39 +7,35 @@ const newNoteContent = `<div class="h-100 new-note-wrapper">
 										<h2 class="new-note-title">Untitled</h2>
 										<button
 											type="button"
-											class="btn btn-md btn-success"
+											class="btn btn-md btn-success save-button"
 										>
 											Save</button
-										><button
-											type="button"
-											class="btn btn-md btn-outline-danger"
 										>
-											Delete
-										</button>
 									</div>
 									<div class="border border-1 rounded-3 p-3">
 										<div class="mb-3">
 											<label
 												for="note-title"
-												class="form-label fw-semibold fs-4"
+												class="form-label fw-semibold fs-4 d-none"
 												>Title</label
 											>
 											<input
-												type="email"
+												type="text"
 												class="form-control"
-												id="note-title"
+												id="note-title-input"
 												placeholder="Untitled"
 											/>
 										</div>
 										<div class="mb-3">
 											<label
 												for="note-text"
-												class="form-label fw-semibold fs-4"
+												class="form-label fw-semibold fs-4 d-none"
 												>Note Text</label
 											>
 											<textarea
 												class="form-control"
 												id="note-text"
+												placeholder="Start writing your note..."
 												rows="16"
 											></textarea>
 										</div>
@@ -108,9 +104,37 @@ const renderNote = (note) => {
 	noteView.innerHTML = currentNote
 }
 
+const getNewNote = () => {
+	const noteTitleInput = document.querySelector('#note-title-input')
+	const newNoteTitle = document.querySelector('.new-note-title')
+	const noteText = document.querySelector('#note-text')
+	const saveButton = document.querySelector('.save-button')
+	noteTitleInput.addEventListener('keyup', (e) => {
+		newNoteTitle.textContent = e.target.value
+	})
+	saveButton.addEventListener('click', (e) => {
+		if (noteTitleInput.value.length === 0 || noteText.value.length === 0) {
+			console.log('No new note to save')
+			return
+		}
+		const newNote = { title: noteTitleInput.value, noteText: noteText.value }
+		saveNote(newNote)
+	})
+}
+
+const saveNote = async (note) => {
+	const url = `http://localhost:5000/api/notes`
+	try {
+		await fetch(url, { method: 'POST', body: JSON.stringify(note), headers: { 'Content-Type': 'application/json' } })
+	} catch (error) {
+		console.log(error)
+	}
+}
+
 const main = () => {
 	getNotes()
 	noteView.innerHTML = newNoteContent
+	getNewNote()
 }
 
 main()
