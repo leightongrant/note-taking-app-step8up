bd3df0fc69f8bed9d625f2a7fbfdf24a8b517b2a
Leighton Grant <oneal.grant@outlook.com>
2025-11-04 10:15:10 +0000

    feat(api): add note update endpoint with PUT method

--- routes.ts
+++ routes.ts
@@ -13,8 +13,14 @@ type NotesData = {
 	id: string
 	title: string
 	noteText: string
-	createdAt: string
-	updatedAt: string
+	createdAt: Date
+	updatedAt: Date
+}
+
+type NotesUpdateData = {
+	title: string
+	noteText: string
+	updatedAt: Date
 }
 
 export const router = Router()
@@ -37,6 +43,7 @@ router.get('/notes/:id', async (req: Request, res: Response) => {
 
 // Create note
 router.post('/notes', async (req: Request, res: Response) => {
+	// Get note data
 	const { body } = req
 	if (!body) {
 		res.status(400).json({
@@ -44,22 +51,28 @@ router.post('/notes', async (req: Request, res: Response) => {
 			message: 'The request body is empty. Please provide valid note data.',
 		})
 	}
+	// Validate note Data
 	const { error, value } = schema.validate(body)
 	if (error) {
 		res.status(400).json(error.message)
 	}
 
+	// Create new note
 	const newNote: NotesData = { id: nanoid(), ...value, createdAt: new Date(), updatedAt: new Date() }
 
+	// Get note data. If no data create new array with note
 	try {
 		const notesData = await readFile(new URL('./notes.json', import.meta.url), 'utf-8')
 		if (!notesData) {
 			await writeFile('./notes.json', JSON.stringify([newNote]))
-			res.status(201).json({ message: 'success' })
+			res.status(201).json({ message: 'Success, new note created' })
 			return
 		}
+
+		// Get all notes data
 		const notes = JSON.parse(notesData) as NotesData[]
 
+		// Check if note with current title already in database
 		const conflict = notes.find((note) => note.title === newNote.title)
 		if (conflict) {
 			res.status(409).json({
@@ -69,11 +82,70 @@ router.post('/notes', async (req: Request, res: Response) => {
 			return
 		}
 
+		// Add new note to array
 		notes.push(newNote)
-
+		// Save new notes
 		await writeFile('./notes.json', JSON.stringify(notes))
 
-		res.status(201).json({ message: 'success' })
+		res.status(201).json({ message: 'Success, new note created' })
+	} catch (error) {
+		res.status(400).json(error)
+	}
+})
+
+// Edit a note
+router.put('/notes/:id', async (req: Request, res: Response) => {
+	const { id } = req.params
+	if (!id) {
+		res.status(400).json({
+			error: 'Empty Request',
+			message: 'The request id is empty. Please provide a valid id.',
+		})
+	}
+	const { body } = req
+	// Check for incoming data
+	if (!body) {
+		res.status(400).json({
+			error: 'Empty Request',
+			message: 'The request body is empty. Please provide valid note data.',
+		})
+	}
+	// Validate incoming data
+	const { error, value } = schema.validate(body)
+	if (error) {
+		res.status(400).json(error.message)
+	}
+	// Create update data object
+	const updateData: NotesUpdateData = { ...value, updatedAt: new Date() }
+
+	try {
+		const notesData = await readFile(new URL('./notes.json', import.meta.url), 'utf-8')
+		const newNote: NotesData = { id: nanoid(), ...updateData, createdAt: new Date() }
+		if (!notesData) {
+			await writeFile('./notes.json', JSON.stringify([newNote]))
+			res.status(201).json({ message: 'Success, new note created' })
+			return
+		}
+		let notes = JSON.parse(notesData) as NotesData[]
+
+		// Return not found if note not found
+		let currentNote = notes.find((note) => note.id === id)
+		if (!currentNote) {
+			res.status(404).json({
+				error: 'Not found',
+				message: `A note with this id(${id}) is not found`,
+			})
+			return
+		}
+		// Update current note
+		currentNote = { ...currentNote, ...updateData }
+		// Remove old note
+		notes = notes.filter((note) => note.id !== id)
+		// Add updated note
+		notes.push(currentNote)
+
+		await writeFile('./notes.json', JSON.stringify(notes))
+		res.status(200).json(currentNote)
 	} catch (error) {
 		res.status(400).json(error)
 	}
